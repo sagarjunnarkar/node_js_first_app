577d935c4a7e14d91a98ec219d975caaf8c7a2f4
Sagar <spjunnarkar@gmail.com>
2015-04-22 13:05:06 +0530

    Basic Routing

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ var express = require('express');
 var app = express();
 
 app.get('/',function (req, res){
-	res.send('Hello World');
+	res.send('Hello GET');
+})
+
+app.post('/',function (req, res){
+  res.send('Hello POST');
+})
+
+app.delete('/del_user', function (req, res){
+  res.send('Hello DELETE');
+})
+
+app.get('/list_user', function (req, res){
+  res.send('Page Listing');
+})
+
+app.get('/ab*cd', function (req, res){
+	res.send('Page Pattern Match')
 })
 
 var server = app.listen(8081, function(){
