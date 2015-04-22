0cb7270c6676f2c084c35cd70f01bb59eef2057b
Sagar <spjunnarkar@gmail.com>
2015-04-22 13:16:15 +0530

    GET Method

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ var app = express();
 
 app.use(express.static('public'))
 
+app.get('/index.htm', function (req, res){
+	res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/process_get', function (req, res){
+	response = {
+		first_name: req.query.first_name,
+		last_name: req.query.last_name
+	};
+	res.end(JSON.stringify(response));
+})
+
 app.get('/',function (req, res){
 	res.send('Hello GET');
 })
